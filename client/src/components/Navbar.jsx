d84c54de60c0b52213fa2215be4c7c39c142201b
Ashutosh Raj Gupta <ag2364443@gmail.com>
2023-07-06 22:51:53 +0530

    code updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -82,7 +82,7 @@ const Navbar = () => {
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>LAMA.</Logo>
+          <Logo>ASHU</Logo>
         </Center>
         <Right>
           <MenuItem>REGISTER</MenuItem>
